f025d2106b93b7184842110f32729d38e2fd44a6
Michael Kamerath <mkamerath@gmail.com>
2017-04-13 16:12:27 -0600

    Updating TeamStatistics class

--- ReadInData.py
+++ ReadInData.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import TeamStatistics as ts
-from operator import itemgetter
-from itertools import groupby as grpby
 import numpy as np
 
 game_stats = pd.read_csv('All_Data/09_14_game_stats.csv')
@@ -19,6 +17,12 @@ all_years = sorted(all_years)
 print(all_teams)
 print(all_years)
 
-unc_2008 = all_stats.ix[(all_stats['team'] == 'North Carolina') & (all_stats['year'] == 2008)]
+unc_2008 = all_stats.ix[((all_stats['team'] == 'North Carolina') | (all_stats['opponent'] == 'North Carolina'))
+                        & (all_stats['year'] == 2008)]
 
-print(unc_2008)
+al_huntsville_2008 = all_stats.ix[
+    ((all_stats['team'] == 'AL-Huntsville') | (all_stats['opponent'] == 'AL-Huntsville'))
+    & (all_stats['year'] == 2008)]
+
+team_stats = ts.TeamStatistics()
+team_stats.setup('North Carolina', 2008, unc_2008)
--- TeamStatistics.py
+++ TeamStatistics.py
@@ -1,9 +1,31 @@
-class Team_Statistics(object):
+from collections import defaultdict
 
+
+class TeamStatistics(object):
+
+    # This class contains statistics about all teams currently
+    # in the data set. The game_stats from the entire season
+    # for each team are kept track of so averages for the season
+    # can be computed. Both of the data structures will have keys
+    # that are the team name and year because together, they determine
+    # what a unique season is for a team.
     def __init__(self):
-        game_stats = {}
+        self.game_stats = defaultdict(lambda: [0, 0])
+        self.season_averages = defaultdict(lambda : [0,0])
+
+    # Adds data obtained from the csv files to this class
+    # It will later be used to calculate averages for the
+    # season.
+    #  prerequisites - none
+    #  arguments:
+    #    team: Name of team
+    #    year: the year being considered when collecting statistics
+    #    frame: the data frame containing information about the team
+    def setup(self, team, year, frame):
+        self.game_stats[team, year].append(frame)
+
+    def create_stats(self, team, year):
+        for key, value, frame in 
 
 
-    def get_Team_Info(self, data):
-        return 0
 
